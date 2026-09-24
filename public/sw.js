b18f43804956762ab8972c1bbf74a0661573371a
ayushydv1805 <ayushyadav0000018@gmail.com>
2026-09-24 20:07:53 +0530

    Phase 12: bump PWA cache namespace

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "voiceprint-shell-v11";
+const CACHE = "voiceprint-shell-v12";
 const CORE = ["/", "/index.html", "/manifest.webmanifest", "/voiceprint-icon.svg"];
 
 self.addEventListener("install", (event) => {
